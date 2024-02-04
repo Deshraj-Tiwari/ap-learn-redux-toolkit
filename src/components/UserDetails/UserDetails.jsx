a0409eeb40a5045f8209272c1927fca17c75ff38
Deshraj Tiwari <deshrajtiwari690@gmail.com>
2024-02-04 09:14:57 +0530

    Displaying and getting users

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import RemoveUsers from "./RemoveUsers/RemoveUsers";
+import { fakeUserData } from "../../api";
+import { useDispatch } from "react-redux";
+import { addUser } from "../../store/slices/UserSlice";
+import DisplayUsers from "./DisplayUsers/DisplayUsers";
 import "./UserDetails.css";
 
 export default function UserDetails() {
+  const dispatch = useDispatch();
+
+  function createUser(fakeUser) {
+    dispatch(addUser(fakeUser));
+  }
   return (
     <>
       <div
@@ -10,88 +19,17 @@ export default function UserDetails() {
         className="container mt-4 d-flex justify-content-between"
       >
         <h5 className="d-inline">List of User Details</h5>
-        <button className="btn btn-sm btn-primary d-inline">
+        <button
+          onClick={() => createUser(fakeUserData())}
+          className="btn btn-sm btn-primary d-inline"
+        >
           Add New User
         </button>
       </div>
       <br />
       <div className="container userDetails d-flex justify-content-center">
         <ul className="list-group list-group-flush">
-          <li className="list-group-item d-flex justify-content-between">
-            An item
-            <button className="btn btn-sm btn-danger">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-trash-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-              </svg>
-            </button>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            A second item
-            <button className="btn btn-sm btn-danger">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-trash-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-              </svg>
-            </button>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            A third item
-            <button className="btn btn-sm btn-danger">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-trash-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-              </svg>
-            </button>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            A fourth item
-            <button className="btn btn-sm btn-danger">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-trash-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-              </svg>
-            </button>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            And a fifth one
-            <button className="btn btn-sm btn-danger">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-trash-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-              </svg>
-            </button>
-          </li>
+          <DisplayUsers />
         </ul>
       </div>
       <hr />
--- src/store/slices/UserSlice.jsx
+++ src/store/slices/UserSlice.jsx
@@ -4,8 +4,12 @@ export const userSlice = createSlice({
   name: "user",
   initialState: [],
   reducers: {
-    addUser(state, action) {},
+    addUser(state, action) {
+      state.push(action.payload);
+    },
     removeUser(state, action) {},
     deleteAll(state, action) {},
   },
 });
+
+export const { addUser } = userSlice.actions;
